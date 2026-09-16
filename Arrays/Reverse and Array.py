b4ad81af85c92daad675ec9b33e667b0c32b2997
shivainlabs <shiva.in.labs@gmail.com>
2026-09-16 10:27:05 +0530

    reverse array

--- Arrays/Reverse and Array.py
+++ Arrays/Reverse and Array.py
@@ -1,2 +1,29 @@
 # https://www.geeksforgeeks.org/problems/reverse-an-array/1
 
+
+"""
+T.C -> O(n/2) ~ O(n)
+S.C -> O(1)
+
+Approach: 
+
+Use two pointer one is at the begining and second at the end, swaping each other
+move i forward and j backward until they meet each other
+
+"""
+
+class Solution:
+    def reverseArray(self, arr):
+        # code here
+        i = 0
+        j = len(arr)-1
+        
+        while i < j:
+            # arr[j],arr[i] = arr[i],arr[j]
+            
+            val = arr[i]
+            arr[i] = arr[j]
+            arr[j] = val
+            
+            i += 1
+            j -= 1        
