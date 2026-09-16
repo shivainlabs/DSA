ab7d636755c076d21b2d8c1dc7da26e4866dc85d
shivainlabs <shiva.in.labs@gmail.com>
2026-09-16 10:43:42 +0530

    added min and max in array

---
+++ Arrays/Min and Max in Array.py
@@ -0,0 +1,25 @@
+class Solution:
+    def getMinMax(self, arr):
+        # code here
+        maxi = arr[0]
+        mini = arr[0]
+        
+        for x in arr[1:]:
+            if x > maxi:
+                maxi = x
+            if x < mini:
+                mini = x
+        
+        return [mini,maxi]
+        
+
+"""
+
+T.C -> O(n)
+S.C -> O(1)
+
+Approach : Storing two variable, and traverse the array updating our
+variables accordingly
+
+
+"""
